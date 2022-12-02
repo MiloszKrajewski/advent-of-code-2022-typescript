66b00817b8259fdfa26fc7e2f76f437f7c131578
Milosz Krajewski <miloszkrajewski@o2.pl>
2022-12-02 14:41:32 +0000

    day1 data loader

--- day01/index.ts
+++ day01/index.ts
@@ -2,7 +2,7 @@ import _ from "lodash";
 import tap from "tap";
 
 import { subtest } from "../tools/TapLike";
-import { loadLines } from "../tools/tools";
+import { loadLines, loadText as loadText } from "../tools/tools";
 
 subtest(tap, "day01", (t) => {
     subtest(t, "example - top 1", t => {
@@ -29,25 +29,15 @@ subtest(tap, "day01", (t) => {
 });
 
 function findTop1(filename: string) {
-    return _([...loadInput(filename)]).maxBy(([_, value]) => value)!;
+    return _(loadInput(filename)).maxBy(([_, value]) => value)!;
 }
 
 function findTop3(filename: string) {
-    return _([...loadInput(filename)]).sortBy(([_, value]) => -value).take(3).sumBy(([_, value]) => value);
+    return _(loadInput(filename)).sortBy(([_, value]) => -value).take(3).sumBy(([_, value]) => value);
 }
 
 function loadInput(filename: string) {
-    const lines = loadLines(filename);
-
-    let group = 1;
-    let stats = new Map<number, number>();
-    for (const line of lines) {
-        if (line == "") {
-            group++;
-        } else {
-            const value = parseInt(line);
-            stats.set(group, (stats.get(group) ?? 0) + value);
-        }
-    };
-    return stats;
+    return loadText(filename)
+        .split(/\r?\n\r?\n/)
+        .map((g, i) => [i + 1, _(g.split(/\r?\n/)).sumBy(parseInt)] as [number, number]);
 }
